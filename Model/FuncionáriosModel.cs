eacbfaeb3aabf61378280918862869701eb77fd0
joaovictor-pro <joaovictor15bazilio@gmail.com>
2025-12-03 23:35:58 -0300

    Create FuncionáriosModel.cs

---
+++ Model/FuncionáriosModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace admTarefas.Model
+{
+    internal class FuncionáriosModel
+    {
+        public string nome { get; set; }
+        public string email { get; set; }
+        public int matricula { get; set; }
+
+        public FuncionáriosModel(string nome, string email, int matricula)
+        {
+            
+            this.nome = nome;
+            this.email = email;
+            this.matricula = matricula;
+
+
+        }
+    }   
+        
+}
