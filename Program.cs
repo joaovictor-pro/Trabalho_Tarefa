2afdf52f8adf50097e914df9678a4ae68d95deca
joaovictor-pro <joaovictor15bazilio@gmail.com>
2025-12-03 23:39:29 -0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Forms;
+using admtarefas.View_;
+
+
+namespace admTarefas
+{
+	static class Program
+	{
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.Run(new FormTarefas()); // ← CORRETO
+		}
+	}
+}
