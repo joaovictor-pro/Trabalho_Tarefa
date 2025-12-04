ede89e0d5e224e4ebcb82917a4b0d8ec3252587b
joaovictor-pro <joaovictor15bazilio@gmail.com>
2025-12-03 23:36:27 -0300

    Create Form1.Designer.cs

---
+++ View_/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace admTarefas
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+     
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            dateTimePicker1 = new DateTimePicker();
+            comboBox1 = new ComboBox();
+            button1 = new Button();
+          
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(213, 21);
+            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(106, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Cadastro de Taefas";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(33, 85);
+            label2.Margin = new Padding(2, 0, 2, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Tarefa";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(213, 85);
+            label3.Margin = new Padding(2, 0, 2, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(31, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Data";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(402, 85);
+            label4.Name = "label4";
+            label4.Size = new Size(39, 15);
+            label4.TabIndex = 3;
+            label4.Text = "Status";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(90, 77);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 4;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(263, 79);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(114, 23);
+            dateTimePicker1.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(463, 77);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(90, 23);
+            comboBox1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(602, 63);
+            button1.Name = "button1";
+            button1.Size = new Size(85, 49);
+            button1.TabIndex = 7;
+            button1.Text = "Cadastrar tarefas";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox1;
+        private DateTimePicker dateTimePicker1;
+        private ComboBox comboBox1;
+        private Button button1;
+    }
+}
