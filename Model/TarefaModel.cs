d704fbb9b9555ac55f4240b11397a376c82b110b
joaovictor-pro <joaovictor15bazilio@gmail.com>
2025-12-03 23:36:08 -0300

    Create TarefaModel.cs

---
+++ Model/TarefaModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace admtarefas.Model
+{
+    public class Tarefa
+    {
+        public string Nome { get; set; }
+        public DateTime Data { get; set; }
+        public string Status { get; set; }
+
+        public Tarefa(string nome, DateTime data, string status)
+        {
+            Nome = nome;
+            Data = data;
+            Status = status;
+        }
+    }
+}
